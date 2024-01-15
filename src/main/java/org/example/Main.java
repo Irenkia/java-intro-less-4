514822118c89744441fd090294726cd128e73163
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-01-15 22:55:15 +0100

    created classe CinemaPlace

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        forCinema();
+    }
+    public static void forCinema(){
+        CinemaPlace place = new CinemaPlace(7, 5);
+        System.out.println("Our seat № " +  place.getPlaceNumber() + " in row " + place.getRowPlace());
+        place.setPlaceStatus(false);
+        boolean status = place.isPlaceStatus();
+        System.out.println("Now our ticket status is free? - " + status);
     }
 }
